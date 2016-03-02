20e8f2cb9f487ec57b9e4415a8fa6e0b14ddb4f3
sirrah23 <sirrah23@users.noreply.github.com>
2016-03-02 00:29:40 -0500

    Wrote tests for getting all polls

--- test/dbtest.js
+++ test/dbtest.js
@@ -24,21 +24,56 @@ describe('Create Poll',function(){
     return Promise.map([question,answer,qa],function(table){
       return table.drop();
     })
+    .then(function(){
+      return Promise.map([question,answer,qa],function(table){
+        return table.sync();
+      });
+    })
+    .then(function(){
+      return dbInstance.createPoll(poll);
+    })
+    .then(function(addedPoll){
+      //make sure we added three answers
+      assert.equal(addedPoll[0].length,3);
+      //Make only added one question with that id of 1
+      assert.deepEqual(addedPoll[0].map(function(added){return added.questionID;}),[1,1,1]);
+      //Make sure all three answers got added with unique ids
+      assert.deepEqual(addedPoll[0].map(function(added){return added.answerID;}),[1,2,3]);
+    })
+    .catch(function(err){
+      console.log(err);
+      assert(false);
+    });
+  });
+});
+
+
+describe('Get all Polls',function(){
+  var poll = [{'question': 'What is this question?', answers: ['answer1','answer2','answer3']},
+              {'question': 'What is that?', answers: ['answer4','answer5','answer6']}];
+  it('should create a poll',function(){
+    return Promise.map([question,answer,qa],function(table){
+      return table.drop();
+    })
       .then(function(){
         return Promise.map([question,answer,qa],function(table){
           return table.sync();
         });
       })
       .then(function(){
-        return dbInstance.createPoll(poll);
+        return dbInstance.createPoll(poll[0]);
       })
-      .then(function(addedPoll){
-        //make sure we added three answers
-        assert.equal(addedPoll[0].length,3);
-        //Make only added one question with that id of 1
-        assert.deepEqual([1,1,1],addedPoll[0].map(function(added){return added.questionID;}));
-        //Make sure all three answers got added with unique ids
-        assert.deepEqual([1,2,3],addedPoll[0].map(function(added){return added.answerID;}));
+      .then(function(){
+        return dbInstance.createPoll(poll[1]);
+      })
+      .then(function(){
+        return dbInstance.getAllPolls();
+      })
+      .then(function(dbPolls){
+        assert.equal(2,dbPolls.length)
+        assert.deepEqual(dbPolls.map(function(p){return p.questionID;}),[1,2]);
+        assert.deepEqual(dbPolls.map(function(p){return p.question;}),[poll[0].question,poll[1].question]);
+        assert.deepEqual(dbPolls.map(function(p){return p.answers;}),[poll[0].answers,poll[1].answers]);
       })
       .catch(function(err){
         console.log(err);
