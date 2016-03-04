50462452dadbc67c59d0ecb4a9eaa32f6306298b
sirrah23 <sirrah23@users.noreply.github.com>
2016-03-03 23:18:51 -0500

    Finished writing database wrapper tests

--- test/dbtest.js
+++ test/dbtest.js
@@ -51,7 +51,7 @@ describe('Create Poll',function(){
 describe('Get all Polls',function(){
   var poll = [{'question': 'What is this question?', answers: ['answer1','answer2','answer3']},
               {'question': 'What is that?', answers: ['answer4','answer5','answer6']}];
-  it('should create a poll',function(){
+  it('should get all polls',function(){
     return Promise.map([question,answer,qa],function(table){
       return table.drop();
     })
@@ -70,10 +70,83 @@ describe('Get all Polls',function(){
         return dbInstance.getAllPolls();
       })
       .then(function(dbPolls){
-        assert.equal(2,dbPolls.length)
+        assert.equal(2,dbPolls.length);
         assert.deepEqual(dbPolls.map(function(p){return p.questionID;}),[1,2]);
         assert.deepEqual(dbPolls.map(function(p){return p.question;}),[poll[0].question,poll[1].question]);
         assert.deepEqual(dbPolls.map(function(p){return p.answers;}),[poll[0].answers,poll[1].answers]);
+      })
+      .catch(function(err){
+        console.log(err);
+        assert(false);
+      });
+  });
+});
+
+describe('Get a poll', function(){
+  it('should get a poll by ID',function(){
+    var poll = [{'question': 'What is this question?', answers: ['answer1','answer2','answer3']},
+                {'question': 'What is that?', answers: ['answer4','answer5','answer6']}];
+    return Promise.map([question,answer,qa],function(table){
+      return table.drop();
+    })
+      .then(function(){
+        return Promise.map([question,answer,qa],function(table){
+          return table.sync();
+        });
+      })
+      .then(function(){
+        return dbInstance.createPoll(poll[0]);
+      })
+      .then(function(){
+        return dbInstance.createPoll(poll[1]);
+      })
+      .then(function(){
+        return dbInstance.getPollByID(1);
+      })
+      .then(function(pollOne){
+        assert.equal(pollOne[0].questionID,1);
+        assert.deepEqual(pollOne[0].question,poll[0].question);
+        assert.deepEqual(pollOne[0].answers,poll[0].answers);
+        return dbInstance.getPollByID(2);
+      })
+      .then(function(pollTwo){
+        assert.equal(pollTwo[0].questionID,2);
+        assert.deepEqual(pollTwo[0].question,poll[1].question);
+        assert.deepEqual(pollTwo[0].answers,poll[1].answers);
+      })
+      .catch(function(err){
+        console.log(err);
+        assert(false);
+      });
+  });
+});
+
+describe('Delete a poll by ID',function(){
+  it('should delete a poll given id',function(){
+    var poll = {'question': 'What is the question?', answers: ['answer1','answer2','answer3']};
+      return Promise.map([question,answer,qa],function(table){
+        return table.drop();
+      })
+      .then(function(){
+        return Promise.map([question,answer,qa],function(table){
+          return table.sync();
+        });
+      })
+      .then(function(){
+        return dbInstance.createPoll(poll);
+      })
+      .then(function(){
+        return dbInstance.getAllPolls();
+      })
+      .then(function(pollNew){
+        assert.equal(pollNew.length,1);
+        return dbInstance.deletePollByID(1);
+      })
+      .then(function(){
+        return dbInstance.getAllPolls();
+      })
+      .then(function(pollNew){
+        assert.equal(pollNew.length,0);
       })
       .catch(function(err){
         console.log(err);
